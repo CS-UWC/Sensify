f40dbbdf5208952d27a243f436cf968cc841333d
pascal <24707044+PascalBenstrong@users.noreply.github.com>
2024-09-02 21:13:59 +0200

    added time endpoint and testing of record method with raw http request body

--- Sensify/Program.cs
+++ Sensify/Program.cs
@@ -12,17 +12,29 @@ app.UseHttpsRedirection();
 
 var api = app.MapGroup("/api");
 
-api.MapPost("/record", (ILoggerFactory loggerFactory, HttpRequest request, [FromBody] Dictionary<string,object> body) =>
+api.MapPost("/record", async (ILoggerFactory loggerFactory, HttpRequest request /*[FromBody] Dictionary<string,object> body*/) =>
 {
-   var logger = loggerFactory.CreateLogger("test");
+   var logger = loggerFactory.CreateLogger("record");
 
     logger.LogInformation("received data");
 
     logger.LogInformation("ContentType: {@ContentType}", request.ContentType);
 
+    var body = await new StreamReader(request.Body).ReadToEndAsync();
+
     logger.LogInformation("Body: {@Body}", body);
 
     return Results.Ok();
+});
+
+api.MapGet("/time", (ILoggerFactory loggerFactory) => {
+    var logger = loggerFactory.CreateLogger("time");
+
+    var utcNow = DateTimeOffset.UtcNow;
+
+    logger.LogInformation("timw: {@time}", utcNow);
+
+    return Results.Ok(utcNow.ToUnixTimeMilliseconds());
 });
 
 app.Run();
