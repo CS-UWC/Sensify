be1c64c4c44abdbbadf7c6fbfe6ae71128640878
pascal <24707044+PascalBenstrong@users.noreply.github.com>
2024-10-13 00:47:05 +0200

    Added sensor type and SensorInfo type

---
+++ Sensify/Grains/ISensor.cs
@@ -0,0 +1,14 @@
+﻿namespace Sensify.Grains;
+
+[Alias("Sensify.Grains.ISensor")]
+public interface ISensor : IGrainWithStringKey
+{
+    Task<string> GetIdAsync();
+    Task<SensorInfo> GetSensorInfoAsync();
+    Task UpdateMeasurementAsync(string hexPayload);
+    Task SetSensorNameAsync(string name);
+    Task SetSensorTypeAsync(string type);
+    Task SetPayloadDecoderAsync(string decoderName);
+    Task<T> GetMeasurementsAsync<T>(SensorMeasurementDateRange dateRange = default, MeasurementWindow window = default);
+
+}
---
+++ Sensify/Grains/SensorInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace Sensify.Grains;
+
+[GenerateSerializer]
+[Alias("Sensify.Grains.SensorInfo")]
+public sealed record class SensorInfo
+{
+    [Id(0)]
+    public string Id { get; set; } = string.Empty;
+    [Id(1)]
+    public string SensorType { get; set; } = string.Empty;
+    [Id(2)]
+    public string SensorName { get; set; } = string.Empty;
+    [Id(3)]
+    public string PayloadDecoder { get; set; } = string.Empty;
+}
