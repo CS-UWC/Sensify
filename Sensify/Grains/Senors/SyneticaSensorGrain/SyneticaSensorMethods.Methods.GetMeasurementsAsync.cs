9406cf6d1897c313c557b5b29f0500133c3f21ef
pascal <24707044+PascalBenstrong@users.noreply.github.com>
2024-10-23 02:40:18 +0200

    Added Synetica sensor implementation

---
+++ Sensify/Grains/Senors/SyneticaSensorGrain/SyneticaSensorMethods.Methods.GetMeasurementsAsync.cs
@@ -0,0 +1,109 @@
+﻿using MongoDB.Driver;
+using Sensify.Decoders.Netvox;
+using Sensify.Decoders.Synetica;
+using Sensify.Extensions;
+using Sensify.Grains.Senors.Common;
+using Sensify.Persistence;
+using System.Runtime.CompilerServices;
+
+namespace Sensify.Grains.SyneticaSensorGrain;
+
+internal sealed partial class SyneticaSensorMethods
+{
+    public IAsyncEnumerable<object> GetMeasurementsAsync(SensorMeasurementDateRange dateRange = default, MeasurementWindow window = default)
+    {
+        List<FilterDefinition<SensorMeasurement<SyneticaMeasurement>>> filters = [];
+
+        var sensorFilter = Builders<SensorMeasurement<SyneticaMeasurement>>.Filter.Eq(x => x.SensorId, _state.State.Id.ToString());
+        filters.Add(sensorFilter);
+
+        if(dateRange != SensorMeasurementDateRange.All)
+        {
+            var startingDateFilter = Builders<SensorMeasurement<SyneticaMeasurement>>.Filter.Gte(x => x.Timestamp, dateRange.Start);
+            filters.Add(startingDateFilter);
+            var endDateFilter = Builders<SensorMeasurement<SyneticaMeasurement>>.Filter.Lte(x => x.Timestamp, dateRange.End);
+            filters.Add(endDateFilter);
+        }
+
+        var combined = Builders<SensorMeasurement<SyneticaMeasurement>>.Filter.And(filters);
+        var sort = Builders<SensorMeasurement<SyneticaMeasurement>>.Sort.Ascending(x => x.Timestamp);
+
+        var findFluent = _measurements.Find(combined)
+            .Sort(sort);
+
+        if (window != MeasurementWindow.None)
+        {
+            return new WindowedAsyncEnumerable(findFluent, window);
+        }
+
+        return findFluent.GetAsyncEnumerable()
+            .Transform(x => (object)x);
+        
+    }
+
+}
+
+file readonly struct WindowedAsyncEnumerable : IAsyncEnumerable<object>
+{
+    private readonly IFindFluent<SensorMeasurement<SyneticaMeasurement>, SensorMeasurement<SyneticaMeasurement>> _findFluent;
+    private readonly MeasurementWindow _window;
+
+    public WindowedAsyncEnumerable(IFindFluent<SensorMeasurement<SyneticaMeasurement>, SensorMeasurement<SyneticaMeasurement>> findFluent, MeasurementWindow window)
+    {
+        _findFluent = findFluent;
+        _window = window;
+    }
+
+    public IAsyncEnumerator<object> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+    {
+
+        var windowedValues = _window.Type switch
+        {
+            MeasurementWindowType.Integral => _findFluent.GetAsyncEnumerator(cancellationToken)
+            .Window(Math.Max(1, Math.Min(60, _window.WindowSize))),
+            _ => _findFluent.GetAsyncEnumerator(cancellationToken)
+            .Window(CreateDurationWindow(TimeSpan.FromMinutes(1).Max(TimeSpan.FromHours(6).Min(_window.WindowDuration))), cancellationToken)
+        };
+
+        return windowedValues.GetAsyncEnumerator(cancellationToken)
+            .Transform(static batch => (object)batch.Aggregate(Aggregate), cancellationToken);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static Func<SensorMeasurement<SyneticaMeasurement>?, SensorMeasurement<SyneticaMeasurement>, IReadOnlyList<SensorMeasurement<SyneticaMeasurement>>, bool>
+            CreateDurationWindow(TimeSpan windowDuration)
+        {
+            return (SensorMeasurement<SyneticaMeasurement>? previous, SensorMeasurement<SyneticaMeasurement> current, IReadOnlyList<SensorMeasurement<SyneticaMeasurement>> accumulated) =>
+            {
+                if (previous is null || (current.Timestamp - previous.Timestamp) < windowDuration) return false;
+
+                return true;
+
+            };
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static SensorMeasurement<SyneticaMeasurement> Aggregate(SensorMeasurement<SyneticaMeasurement> left, SensorMeasurement<SyneticaMeasurement> right)
+        {
+
+            if(left is null)
+            {
+                return right;
+            }
+
+            if(right is null)
+            {
+                return left;
+            }
+
+            left.Timestamp = right.Timestamp;
+            left.SensorId ??= right.SensorId;
+            left.Measurement += right.Measurement;
+
+            return left;
+        }
+
+
+    }
+
+}
---
+++ Sensify/Grains/Senors/SyneticaSensorGrain/SyneticaSensorMethods.Methods.cs
@@ -0,0 +1,68 @@
+﻿using MongoDB.Driver;
+using Sensify.Decoders.Synetica;
+using Sensify.Grains.Senors.Common;
+using Sensify.Persistence;
+
+namespace Sensify.Grains.SyneticaSensorGrain;
+
+internal sealed partial class SyneticaSensorMethods : ISensorMethods
+{
+    private readonly IPersistentState<SensorInfo> _state;
+    private readonly IMongoPersistenceProvider _persistenceProvider;
+    private readonly IGrainContext _grainContext;
+    private readonly SyneticaDecoder _decoder = new();
+    private readonly IMongoCollection<SensorMeasurement<SyneticaMeasurement>> _measurements;
+
+    public SyneticaSensorMethods(
+        IPersistentState<SensorInfo> state,
+        IMongoPersistenceProvider persistenceProvider,
+        IGrainContext grainContext)
+    {
+        _state = state;
+        _persistenceProvider = persistenceProvider;
+        _grainContext = grainContext;
+        _measurements = _persistenceProvider.GetCollection<SensorMeasurement<SyneticaMeasurement>>("sensorData");
+    }
+
+    public ValueTask<string> GetIdAsync()
+    {
+        return ValueTask.FromResult(_state.State.Id.ToString()!);
+    }
+
+    public ValueTask<SensorInfo> GetSensorInfoAsync()
+    {
+        return ValueTask.FromResult(_state.State);
+    }
+
+    public async ValueTask UpdateMeasurementAsync(RawSensorMeasurement raw)
+    {
+        //Console.WriteLine($"hexPayload: {hexPayload}");
+
+        var data = _decoder.Decode(raw.HexPayload);
+
+        if (data is null) return;
+
+        SensorMeasurement<SyneticaMeasurement> sensorData = new()
+        {
+            SensorId = _state.State.Id.ToString()!,
+            Timestamp = raw.Timestamp ?? DateTime.UtcNow,
+            Measurement = data
+        };
+
+        await _measurements.InsertOneAsync(sensorData);
+    }
+
+    public async ValueTask UpdateSensorInfoAsync(UpdateSensorInfo update)
+    {
+        var state = _state.State;
+
+        _state.State = state with
+        {
+            SensorName = update.SensorName,
+            PayloadDecoder = update.PayloadDecoder ?? state.PayloadDecoder,
+            SensorType = update.SensorType ?? state.SensorType,
+        };
+
+        await _state.WriteStateAsync();
+    }
+}
